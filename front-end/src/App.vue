7cf9bf694fe208e0d13752506495e4e68fa5aff7
Denis Rodrigues <iran.dns@hotmail.com>
2023-02-07 21:38:49 -0300

    primeiro commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <ul>
+  
+    <li><router-link :to="{name: 'home'}">Home</router-link></li>
+    <li><router-link :to="{name: 'login'}">Login</router-link></li>
+    <li> <router-link :to="{name: 'dashboard'}">Dashboard</router-link></li>
+
+  </ul> 
+
+  <router-view/>
+  
+</template>
+
+<script defaul>
+
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <h2>Login</h2>
+
+  <form @submit.prevent="Login">
+  
+    <input type="text" placeholder="Seu email">
+    <input type="password" placeholder="Sua Senha">
+    <button type="submit">Login</button>
+
+  </form>
+  
+</template>
+
+<script setup>
+
+function Login(){
+  alert('Login')
+}
+
+</script>
+
+
+<style>
+
+</style>
