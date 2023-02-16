b7bb67631d558266b62b8cb062f9578178cf2316
dnsrodrigues <iran.dns@hotmail.com>
2023-02-16 15:47:07 -0300

    alteração no app.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,21 +1,17 @@
 <template>
+  <div>
+    <ul>
+      <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+      <li><router-link :to="{ name: 'login' }">Login</router-link></li>
+      <li><router-link :to="{ name: 'dashboard' }">Dashboard</router-link></li>
+    </ul>
 
-  <ul>
-  
-    <li><router-link :to="{name: 'home'}">Home</router-link></li>
-    <li><router-link :to="{name: 'login'}">Login</router-link></li>
-    <li> <router-link :to="{name: 'dashboard'}">Dashboard</router-link></li>
-
-  </ul> 
-
-  <router-view/>
-  
+    <router-view />
+  </div>
 </template>
 
 <script defaul>
-
 </script>
 
 <style>
-
 </style>
